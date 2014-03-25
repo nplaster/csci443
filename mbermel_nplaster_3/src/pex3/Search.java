adabc097a25c2ddf3ad942a39212c27384438768
unknown <mbermel@MZ022-02.adit.mines.edu>
2014-03-25 12:41:12 -0600

    Added basic search bar and button (dummies only)

--- mbermel_nplaster_3/src/pex3/Search.java
+++ mbermel_nplaster_3/src/pex3/Search.java
@@ -1,14 +1,36 @@
 package pex3;
 
 import java.applet.Applet;
+
 import java.awt.*;
 import java.awt.event.*;
 
-public class Search extends Applet {
-	Button garbage;
-	
-	public void init(){
-		garbage =  new Button("GARBAGIO");
-		this.add(garbage);
-	}
+
+public class Search extends Applet  { 
+    TextField inputLine = new TextField(15); 
+    Button enterButton = new Button("Enter");
+    
+    public Search() {
+    	setSize(1000, 500);
+        add(inputLine);
+        inputLine.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    String s = inputLine.getText();
+                    String sUp = s.toUpperCase();
+                    inputLine.setText(sUp);
+                }
+            }
+         );
+        add(enterButton);
+        enterButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent event) {
+                        String s = inputLine.getText();
+                        String sUp = s.toUpperCase();
+                        inputLine.setText(sUp);
+                    }
+                }
+             );
+    }
 }
