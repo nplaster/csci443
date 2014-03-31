15b3a0b2156563a8cd939068e6523ac150e6778e
unknown <mbermel@LB266-13.adit.mines.edu>
2014-03-31 14:26:58 -0600

    Added features to Movies.java

--- mbermel_nplaster_3/src/pex3/Movies.java
+++ mbermel_nplaster_3/src/pex3/Movies.java
@@ -1,6 +1,7 @@
 package pex3;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,6 +20,7 @@ public class Movies extends JPanel implements ListSelectionListener {
 
 	public Movies(int movieID){
 		super(new BorderLayout());
+		
 		DefaultListModel<String> listModel;
 	    JList<String> list;
 	    
@@ -27,13 +29,33 @@ public class Movies extends JPanel implements ListSelectionListener {
         try{
             con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/sakila", "root", "" );
             Statement stmt = con.createStatement();
-
+            JPanel skeleton = new JPanel(new GridLayout(4,0));
+            add(skeleton, BorderLayout.CENTER);
             ResultSet rs = stmt.executeQuery( "select * from film_text where film_id=" +movieID);
             rs.next();
+            String title = rs.getString("title");
             JLabel movieTitle = new JLabel(rs.getString("title"));
             add(movieTitle, BorderLayout.NORTH);
+            JLabel description = new JLabel("Movie Description:");
+            skeleton.add(description);
             JLabel movieDescription = new JLabel(rs.getString("description"));
-            add(movieDescription, BorderLayout.CENTER);
+            skeleton.add(movieDescription);
+            
+            rs = stmt.executeQuery("select * from film_list where title='" +title+ "'");
+            rs.next();
+            String[] actorList = rs.getString("actors").replaceAll("^[,\\s]+", "").split(",");
+            
+            for(int i = 0; i<actorList.length; i++){
+            	listModel.addElement(actorList[i]);
+            }
+            
+            list = new JList<String>(listModel);
+    		list.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+    		list.addListSelectionListener( this );
+            
+            JLabel actors = new JLabel("Actors in this film:");
+            skeleton.add(actors);
+            skeleton.add(new JScrollPane( list ));
             
             rs.close();
             stmt.close();
